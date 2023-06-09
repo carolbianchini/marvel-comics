cfe21c442ce62bbd52e61c910ea364cf9d55ce5f
carrottown <anabianchini@outlook.com.br>
2023-06-09 06:22:10 -0300

    mamipulando o Dom

--- JS/app.js
+++ JS/app.js
@@ -5,5 +5,31 @@ const md5 = 'bac1217b8540f7845d08273bb6f3a25b'
 fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}`).then((response) => {
     return response.json();
 }).then((jsonParsed) => {
+    const divHero = document.getElementById('herois');
+
+    jsonParsed.data.results.forEach(element => {
+        const srcImage = element.thumbnail.path + '.' + element.thumbnail.extension
+        const nameHero = element.name
+
+        createDivHreo(srcImage, nameHero, divHero);
+    });
+
     console.log(jsonParsed);
 })
+
+function createDivHreo(srcImage, nameHero, divToAppend) {
+    const divPai = document.createElement('div')
+    const divFilho = document.createElement('div')
+    const textName = document.createElement('text')
+    const img = document.createElement('img')
+
+    textName.textContent = nameHero
+    img.src = srcImage
+
+    divFilho.appendChild(img)
+    divFilho.appendChild(textName)
+    divPai.appendChild(divFilho)
+    divToAppend.appendChild(divPai)
+
+    divPai.classList.add('personagem');
+}
